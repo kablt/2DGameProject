d9472470033fa73dcd86a6f3d7ebda25c24dfcd2
kablt <152235309+kablt@users.noreply.github.com>
2024-05-06 18:28:48 +0900

    상점 ui 제작 및 상점 기능 구현
    캔버스 밖에 상점 조작시 화면에 나올 ui제작/ 조건 달성시 해당화면 플레이어 화면중앙으로 이동하는 함수 구현.
    상점 기능 추가 돈 부족시 멘트 변경

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -41,6 +41,7 @@ public class Player : MonoBehaviour
     bool isreload;
     bool isBorder;
     bool isDamage;
+    bool isShop;
 
     bool sDown1;
     bool sDown2;
@@ -164,7 +165,7 @@ public class Player : MonoBehaviour
         isFireReady = equipWeapon.rate < fireDelay;
         
 
-        if (fDown && isFireReady && !isDodge && !isSwap)
+        if (fDown && isFireReady && !isDodge && !isSwap && !isShop)
         {
             equipWeapon.Use();
             anim.SetTrigger(equipWeapon.type == Weapon.Type.Melee? "doSwing": "doShot");
@@ -207,7 +208,7 @@ public class Player : MonoBehaviour
             return;
         if (ammo == 0)
             return;
-        if(rDown && !isJump && !isDodge && !isSwap && isFireReady)
+        if(rDown && !isJump && !isDodge && !isSwap && isFireReady && !isShop)
         {
             anim.SetTrigger("doReload");
             isreload = true;
@@ -226,7 +227,7 @@ public class Player : MonoBehaviour
 
     void Dodge()
     {
-        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge && !isSwap)
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge && !isSwap && !isShop)
         {
             dodgeVec = moveVec;
             speed *= 2;
@@ -256,7 +257,7 @@ public class Player : MonoBehaviour
             if (sDown1)weaponIndex = 0;
             if (sDown2)weaponIndex = 1;
             if (sDown3)weaponIndex = 2;
-        if ((sDown1 || sDown2 || sDown3) && !isJump && !isDodge)
+        if ((sDown1 || sDown2 || sDown3) && !isJump && !isDodge && !isShop)
         {
             if(equipWeapon !=null)
             equipWeapon.gameObject.SetActive(false);
@@ -289,6 +290,12 @@ public class Player : MonoBehaviour
                 Debug.Log("eeeeeee");
 
                 Destroy(nearObject);
+            }
+            else if(nearObject.tag == "Shop")
+            {
+                Shop shop = nearObject.GetComponent<Shop>();
+                shop.Enter(this);
+                isShop = true;
             }
         }
     }
@@ -397,7 +404,7 @@ public class Player : MonoBehaviour
 
     void OnTriggerStay(Collider other)
     {
-        if(other.tag == "Weapon")
+        if(other.tag == "Weapon" || other.tag =="Shop")
         {
             nearObject = other.gameObject;
 
@@ -410,6 +417,13 @@ public class Player : MonoBehaviour
         if (other.tag == "Weapon")
         {
             nearObject = null;
+        }
+        else if(other.tag =="Shop")
+        {
+            Shop shop = nearObject.GetComponent<Shop>();
+            shop.Exit();
+            nearObject = null;
+            isShop = false;
         }
     }
 
